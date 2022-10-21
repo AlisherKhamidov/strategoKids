b6fccbe5aa9255543e1fc25857d986054b3e1ce3
Alisher Khamidov <a.a.khamidov@yandex.ru>
2022-10-21 15:21:56 +0300

    release1:Banner,Stories

--- frontend/src/features/main/MainPage.tsx
+++ frontend/src/features/main/MainPage.tsx
@@ -1,9 +1,11 @@
 import Banner from '../banner/Banner';
+import Stories from '../stories/Stories';
 
 export default function MainPage(): JSX.Element {
   return (
     <>
       <Banner />
+      <Stories />
       <h1>это главная страница</h1>
     </>
   );
