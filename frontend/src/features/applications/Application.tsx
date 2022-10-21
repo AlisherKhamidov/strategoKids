342c9cb8bf8884859c7b0175eac90a28e83d970a
АНЖЕЛИКА <anzelika@MacBook-Air-ANZELIKA.local>
2022-10-21 15:15:53 +0300

    release1: Application Form done

--- frontend/src/features/applications/Application.tsx
+++ frontend/src/features/applications/Application.tsx
@@ -1,6 +1,76 @@
-export default function Application(): JSX.Element {
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import 'semantic-ui-css/semantic.min.css';
+import { Form, Input, TextArea, Button } from 'semantic-ui-react';
+import style from './Application.module.css';
 
+interface Data {
+  kidName: string;
+  birthDate: string;
+  parentName: string;
+  phone: string;
+  experience: string;
+}
+
+export default function Application(): JSX.Element {
+  const { register, handleSubmit } = useForm<Data>();
+
+  function onSubmit(data: Data): void {
+    console.log(data);
+    }
   return (
-    <h1>Подача заявки</h1>
+    <div className={style.formochka}>
+    <Form onSubmit={handleSubmit(onSubmit)}>
+    <Form.Group widths="equal">
+      <Form.Field
+        id="form-input-control-first-name"
+        control={Input}
+        label="Имя ребёнка"
+        placeholder="Введите имя ребёнка"
+      >
+        <input {...register('kidName')} required />
+      </Form.Field>
+      <Form.Field
+        id="form-input-control-last-name"
+        control={Input}
+        label="Дата рождения ребёнка"
+        placeholder="00.00.0000"
+      >
+          <input {...register('birthDate')} required />
+      </Form.Field>
+      <Form.Field
+        id="form-input-control-last-name"
+        control={Input}
+        label="Имя родителя"
+        placeholder="Введите ваше имя"
+      >
+          <input {...register('parentName')} required />
+      </Form.Field>
+      <Form.Field
+        id="form-input-control-last-name"
+        control={Input}
+        label="Телефон"
+        placeholder="+7-000-000-00-00"
+      >
+          <input {...register('phone')} required />
+      </Form.Field>
+    </Form.Group>
+    <Form.Field
+      id="form-input-control-last-name"
+      control={Input}
+      label="Опыт"
+      placeholder="Опишите опыт вашего ребёнка в шахматах"
+    >
+        <input {...register('experience')} />
+    </Form.Field>
+    <Form.Field
+      id="form-input-control-last-name"
+      control={Button}
+      content="Отправить заявку"
+      className={style.my_button}
+      type="submit"
+    />
+    </Form>
+    </div>
   );
 }
