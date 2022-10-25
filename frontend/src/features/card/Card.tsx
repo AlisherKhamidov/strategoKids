e8596671bde88f88c73ab3046832a4441db795d2
Alisher Khamidov <a.a.khamidov@yandex.ru>
2022-10-26 00:02:05 +0300

    footer-and-tournaments

--- frontend/src/features/card/Card.tsx
+++ frontend/src/features/card/Card.tsx
@@ -24,24 +24,24 @@ export default function Card({ photo, title, description = '', modal = true, lin
 
   return (
         toggle ? (
-        <div
-          className={cardStyle.card}
-          style={{ backgroundImage: `url(${photo})`,
-                  backgroundSize: 'contain',
-                  // cover
-                  backgroundRepeat: 'noRepeat', }}
-        >
-        <div className={cardStyle.title}>{title}</div>
+      <div className={cardStyle.card}>
+        <div className={cardStyle.titleBox}>
+          <div className={cardStyle.title}>{title}</div>
+        </div>
+        <img src={photo} className={cardStyle.image} alt="" />
         <button className={cardStyle.whiteBtn} type="button" onClick={modal ? toggleHandler(toggle) : () => navigate(link)}>Узнать</button>
-        </div>
+      </div>
 ) : (
-        <div className={cardStyle.cardBlue}>
-        <div className={cardStyle.title}>{title}</div>
+      <div className={cardStyle.cardBlue}>
+        <div className={cardStyle.titleBox}>
+          <div className={cardStyle.title}>{title}</div>
+        </div>
         <div className={cardStyle.text}>
-          <p>{description}</p>
+          {description}
         </div>
-        <button className={cardStyle.whiteBtn} type="button" onClick={toggleHandler(toggle)}>Узнать</button>
-        </div>
+        <button className={cardStyle.whiteBtn} type="button" onClick={toggleHandler(toggle)}>Назад</button>
+      </div>
+
       )
   );
 }
