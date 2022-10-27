85c39fc02db6ec2a6b881d67716670e5bb734877
Max Popov <maxpopov19@icloud.com>
2022-10-27 16:29:25 +0300

    tournaments mapping main done

--- frontend/src/features/tournaments/Tournaments.tsx
+++ frontend/src/features/tournaments/Tournaments.tsx
@@ -1,4 +1,6 @@
 import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
 import tournamentsStyle from './Tournaments.module.css';
 import kid1 from './images/kid1.jpeg';
 import kid2 from './images/kid2.jpeg';
@@ -6,8 +8,17 @@ import kid3 from './images/kid3.jpeg';
 import tourImage from './images/IMG_1485.jpeg';
 import Arrow from '../arrow/Arrow';
 import TournamentCard from '../tournamentCard/TournamentCard';
+import { RootState, useAppDispatch } from '../../store';
+import { loadEvents } from '../events/eventsSlice';
 
 export default function Tournaments(): JSX.Element {
+  const eventsList = useSelector((state: RootState) => state.events.eventsArr);
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(loadEvents());
+  }, [dispatch]);
+  console.log(eventsList);
+
   const navigate = useNavigate();
   return (
     <>
@@ -24,24 +35,19 @@ export default function Tournaments(): JSX.Element {
       </button>
     </div>
     <div className={tournamentsStyle.container2}>
-        <TournamentCard
-          photo={kid1}
-          title="Турнир дошкольников"
-          description="Дети начинают с изучения правил. 3000 руб. — 4 занятия по 60 минут за текущий месяц
-    5000 руб. — 8 занятий по 60 минут за текущий месяц"
-        />
-        <TournamentCard
-          photo={kid2}
-          title="Осенний турнир школьников"
-          description="Дети начинают с изучения правил. 3000 руб. — 4 занятия по 60 минут за текущий месяц
-    5000 руб. — 8 занятий по 60 минут за текущий месяц"
-        />
-        <TournamentCard
-          photo={kid3}
-          title="Турнир в ДК"
-          description="Дети начинают с изучения правил. 3000 руб. — 4 занятия по 60 минут за текущий месяц
-    5000 руб. — 8 занятий по 60 минут за текущий месяц"
-        />
+
+   { eventsList.map((event) => {
+if (event.isTournament) {
+return (
+      <TournamentCard
+        key={event.id}
+        photo={event.photo}
+        title={event.title}
+        description={event.description}
+      />
+    );
+}
+}).reverse().slice(0, 5)}
     </div>
     </>
   );
